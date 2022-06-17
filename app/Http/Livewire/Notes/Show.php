8f8e0e856efe06032741f7a8a3c228383fd89adb
abdasis <id.abdasis@gmail.com>
2022-06-18 06:28:19 +0700

    📚 Documentation : Tambah komponen notes

---
+++ app/Http/Livewire/Notes/Show.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Notes;
+
+use Livewire\Component;
+
+class Show extends Component
+{
+    public function render()
+    {
+        return view('livewire.notes.show');
+    }
+}
